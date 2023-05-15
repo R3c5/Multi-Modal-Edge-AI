55e47e0b805dd0042ab900949d7e3a6a505cb425
Sebastian Nechita <89786261+SebiNechita@users.noreply.github.com>
2023-05-15 20:21:28 +0200

    fix: solved type error

--- multi_modal_edge_ai/anomaly_detection/data_access_objects/ArubaDAO.py
+++ multi_modal_edge_ai/anomaly_detection/data_access_objects/ArubaDAO.py
@@ -1,10 +1,11 @@
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+from typing import Any, Callable, Optional
 
 
-class ArubaDAO(Dataset):
-    def __init__(self, csv_path: str, transform=None):
+class ArubaDAO(Dataset[torch.Tensor]):
+    def __init__(self, csv_path: str, transform: Optional[Callable[..., Any]] = None) -> None:
         self.data = pd.read_csv(csv_path)
         self.transform = transform
 
@@ -13,4 +14,4 @@ class ArubaDAO(Dataset):
 
     def __getitem__(self, index: int) -> torch.Tensor:
         sample = self.data.iloc[index]
-        return sample
+        return torch.Tensor(sample)
