40c24c7598c87932fd7b66e5b19c739f44ce7d08
atacettin <E.A.Tacettin@student.tudelft.nl>
2023-05-17 15:59:42 +0200

    fix: added library to requirements feat: added __init__ to abstract class Model docs: added docstring to model.py

--- multi_modal_edge_ai/commons/model.py
+++ multi_modal_edge_ai/commons/model.py
@@ -7,13 +7,26 @@ from pandas import DataFrame, Series
 
 class Model(ABC):
     @abstractmethod
+    def __int__(self, model: Any) -> None:
+        self.model = model
+
+    @abstractmethod
     def train(self, dataset: Union['DataLoader[Any]', DataFrame], **hyperparams: Any) -> None:
+        """
+        abstract method in order to train a model on a dataset, with any hyperparams needed
+        """
         pass
 
     @abstractmethod
     def predict(self, instance: Union[Tensor, 'Series[Any]']) -> Any:
+        """
+        abstract method to predict an instance using the model
+        """
         pass
 
     @abstractmethod
     def save(self, file_path: str) -> None:
+        """
+        abstract method to save the model at a given file path
+        """
         pass
