c169eb08ef579bb05f778b3b4919733c9335893c
Razvan Nistor <r.i.nistor@student.tudelft.nl>
2023-05-17 16:43:21 +0200

    refactor: Remove unused import

--- tests/adl_inference_tests/window_splitter_test.py
+++ tests/adl_inference_tests/window_splitter_test.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from pandas.testing import assert_frame_equal
 from multi_modal_edge_ai.adl_inference.parser import parse_file
 from multi_modal_edge_ai.adl_inference.window_splitter import *
