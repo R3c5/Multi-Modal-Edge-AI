f3d427a6967a7b6392824c0dd9a1ea659a8ca488
Sebastian Nechita <89786261+SebiNechita@users.noreply.github.com>
2023-05-19 22:46:15 +0200

    fix: Pipeline

--- tests/anomaly_detection/synthetic_anomaly_generation_test.py
+++ tests/anomaly_detection/synthetic_anomaly_generation_test.py
@@ -4,14 +4,14 @@ from multi_modal_edge_ai.anomaly_detection.parser import *
 from multi_modal_edge_ai.anomaly_detection.window_splitter import *
 from multi_modal_edge_ai.anomaly_detection.synthetic_anomaly_generator import *
 
+
 def test_check_number_anomalous_windows_aruba():
-    df = parse_file_without_idle("/Users/alexandru-sebastian-nechita/UNI/SP/multi-modal-edge-ai/multi_modal_edge_ai/public_datasets/Aruba_Idle_Squashed.csv")
+    df = parse_file_without_idle("multi_modal_edge_ai/public_datasets/Aruba_Idle_Squashed.csv")
 
     # Check number of anomalous windows
     windows = split_into_windows(df, 3, 2)
 
     (normal_windows, anomalous_windows) = clean_windows(df, windows, event_based=True)
 
-    print(anomalous_windows)
     assert len(anomalous_windows) == 1097
-    assert  len(normal_windows) == 3715
+    assert len(normal_windows) == 3715
