b3d42f76f9770206601b57118e76468ab2d4d7b9
Rafael Borges <R.BragaMedeirosMotaBorges@student.tudelft.nl>
2023-05-24 17:30:04 +0200

    feat: Add functions to convert categorical to numerical and standard scaling of dataframe

---
+++ multi_modal_edge_ai/anomaly_detection/preprocessing/adl_preprocessing.py
@@ -0,0 +1,45 @@
+from typing import Tuple
+
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
+
+def dataframe_categorical_to_numeric(df: pd.DataFrame, window_size, one_hot=True) -> Tuple[pd.DataFrame, int]:
+    distinct_adl_list = pd.unique(data.iloc[:, 2::3].values.ravel('K'))
+
+    if one_hot:
+        encoding_function = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+        encoding_function.fit([[i] for i in distinct_adl_list])
+    else:
+        encoding_function = LabelEncoder()
+        encoding_function.fit(distinct_adl_list)
+
+    new_number_features = 4 + len(distinct_adl_list) if one_hot else 5
+    return (df.apply(lambda row: window_categorical_to_numeric(row, window_size, encoding_function, one_hot), axis=1),
+            new_number_features)
+
+
+def window_categorical_to_numeric(window, window_size, adl_encoding, encoding_type):
+    reshaped_window = pd.DataFrame(window.values.reshape((window_size, 3)))  # three for start_time, end_time, adl_type
+    result = reshaped_window.apply(lambda adl: adl_categorical_to_numeric(adl, adl_encoding, encoding_type), axis=1)
+
+    return pd.Series(result.to_numpy().flatten())
+
+
+def adl_categorical_to_numeric(adl, adl_encoding, one_hot):
+    start_time, end_time, adl_type = adl[0], adl[1], adl[2]
+    duration = (end_time - start_time).total_seconds()
+    encoded = adl_encoding.transform([[adl_type]])[0] if one_hot else adl_encoding.transform([adl_type])
+
+    return pd.Series([start_time.dayofweek, start_time.hour, end_time.hour, duration, *encoded])
+
+
+def dataframe_standard_scaling(df: pd.DataFrame, n_features):
+    original_shape = df.shape
+
+    reshaped_df = pd.DataFrame(df.values.reshape((-1, n_features)))
+    rescaled_df = MinMaxScaler().fit_transform(reshaped_df)
+
+    return pd.DataFrame(rescaled_df.reshape(original_shape), columns=df.columns, index=df.index)
+
