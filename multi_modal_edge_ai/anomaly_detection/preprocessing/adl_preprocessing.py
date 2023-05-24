042019335526743021ae15d1f80ec7c313e74e5c
Rafael Borges <R.BragaMedeirosMotaBorges@student.tudelft.nl>
2023-05-24 17:35:51 +0200

    fix: Add type hints and scaling function

--- multi_modal_edge_ai/anomaly_detection/preprocessing/adl_preprocessing.py
+++ multi_modal_edge_ai/anomaly_detection/preprocessing/adl_preprocessing.py
@@ -1,12 +1,12 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 
-def dataframe_categorical_to_numeric(df: pd.DataFrame, window_size, one_hot=True) -> Tuple[pd.DataFrame, int]:
-    distinct_adl_list = pd.unique(data.iloc[:, 2::3].values.ravel('K'))
+def dataframe_categorical_to_numeric(df: pd.DataFrame, window_size: int, one_hot=True) -> Tuple[pd.DataFrame, int]:
+    distinct_adl_list = pd.unique(df.iloc[:, 2::3].values.ravel('K'))
 
     if one_hot:
         encoding_function = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
@@ -20,14 +20,16 @@ def dataframe_categorical_to_numeric(df: pd.DataFrame, window_size, one_hot=True
             new_number_features)
 
 
-def window_categorical_to_numeric(window, window_size, adl_encoding, encoding_type):
+def window_categorical_to_numeric(window: pd.Series, window_size: int,
+                                  adl_encoding: Union[LabelEncoder | OneHotEncoder], encoding_type: bool) -> pd.Series:
     reshaped_window = pd.DataFrame(window.values.reshape((window_size, 3)))  # three for start_time, end_time, adl_type
     result = reshaped_window.apply(lambda adl: adl_categorical_to_numeric(adl, adl_encoding, encoding_type), axis=1)
 
     return pd.Series(result.to_numpy().flatten())
 
 
-def adl_categorical_to_numeric(adl, adl_encoding, one_hot):
+def adl_categorical_to_numeric(adl: pd.Series, adl_encoding: Union[LabelEncoder | OneHotEncoder],
+                               one_hot: bool) -> pd.Series:
     start_time, end_time, adl_type = adl[0], adl[1], adl[2]
     duration = (end_time - start_time).total_seconds()
     encoded = adl_encoding.transform([[adl_type]])[0] if one_hot else adl_encoding.transform([adl_type])
@@ -35,11 +37,10 @@ def adl_categorical_to_numeric(adl, adl_encoding, one_hot):
     return pd.Series([start_time.dayofweek, start_time.hour, end_time.hour, duration, *encoded])
 
 
-def dataframe_standard_scaling(df: pd.DataFrame, n_features):
+def dataframe_standard_scaling(df: pd.DataFrame, n_features: int) -> pd.DataFrame:
     original_shape = df.shape
 
     reshaped_df = pd.DataFrame(df.values.reshape((-1, n_features)))
     rescaled_df = MinMaxScaler().fit_transform(reshaped_df)
 
     return pd.DataFrame(rescaled_df.reshape(original_shape), columns=df.columns, index=df.index)
-
