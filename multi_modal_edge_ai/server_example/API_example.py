92dd6203e010b2b8e7f635122de4ada03efedc38
Razvan Nistor <r.i.nistor@student.tudelft.nl>
2023-05-27 22:13:03 +0200

    feat: Implement and document example APIs

---
+++ multi_modal_edge_ai/server_example/API_example.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+# GET API
+@app.route('/api/get_greeting', methods=['GET'])
+def get_greeting():
+    name = request.args.get('name')
+    if name:
+        return f"Hello, {name}!"
+    else:
+        return "No name provided."
+
+
+# POST API
+@app.route('/api/post_feedback', methods=['POST'])
+def post_feedback():
+    data = request.json
+    if 'message' in data:
+        name = data['name']
+        message = data['message']
+        print(f'Feedback for {name}: {message}')
+        # Process the message or store it in a database
+        return "Feedback received successfully!"
+    else:
+        return "Invalid feedback data."
