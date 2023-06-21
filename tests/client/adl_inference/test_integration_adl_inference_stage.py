f1193e2b0d6ac7ac29472339ab85edf1092a9283
Sebastian Nechita <89786261+SebiNechita@users.noreply.github.com>
2023-06-21 17:31:34 +0200

    feat: Add pytest fixture to adl stage test

--- tests/client/adl_inference/test_integration_adl_inference_stage.py
+++ tests/client/adl_inference/test_integration_adl_inference_stage.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+import pytest
 
 from multi_modal_edge_ai.client.adl_inference.adl_inference_stage import adl_inference_stage
 from multi_modal_edge_ai.client.common.adl_model_keeper import ADLModelKeeper
@@ -8,21 +9,27 @@ from multi_modal_edge_ai.commons.string_label_encoder import StringLabelEncoder
 from multi_modal_edge_ai.models.adl_inference.ml_models.svm_model import SVMModel
 
 
-def test_adl_inference_stage():
-
+@pytest.fixture
+def adl_model_keeper_fixture():
     # Create a sensor DB
     distinct_adl_list = ['Toilet', 'Relax', 'Kitchen_Usage', 'Sleeping', 'Idle', 'Meal_Preparation', 'Outside',
                          'Movement']
     adl_encoder = StringLabelEncoder(distinct_adl_list)
     adl_model = SVMModel()
-    root_directory = os.path.abspath(os.path.dirname(__file__))
-    adl_model_path = os.path.join(root_directory, 'adl_model')
-    adl_encoder_path = os.path.join(root_directory, 'adl_encoder')
+    root_directory = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    adl_model_path = os.path.join(root_directory, 'model_data', 'adl_model')
+    adl_encoder_path = os.path.join(root_directory, 'model_data', 'adl_encoder')
     adl_model_keeper = ADLModelKeeper(adl_model, adl_model_path, adl_encoder, adl_encoder_path)
     adl_model_keeper.load_encoder()
     adl_model_keeper.load_model()
 
-    prediction = adl_inference_stage(adl_model_keeper, "coho-edge-ai-test", 300, pd.Timestamp('2023-06-19 22:01:00'))
+    return adl_model_keeper
+
+
+def test_adl_inference_stage(adl_model_keeper_fixture):
+
+    prediction = adl_inference_stage(adl_model_keeper_fixture, "coho-edge-ai-test",
+                                     300, pd.Timestamp('2023-06-19 22:01:00'))
 
     assert len(prediction) == 3
     assert prediction['Start_Time'] == pd.Timestamp('2023-06-19 21:56:00')
