0cb22c035e63e7b9c30f0d6f9880d6730ac3a4c4
atacettin <E.A.Tacettin@student.tudelft.nl>
2023-06-23 08:36:47 +0200

    refactor: changed/fixed sensor query for past x seconds of sensor data need to fix tests

--- multi_modal_edge_ai/client/databases/sensor_queries.py
+++ multi_modal_edge_ai/client/databases/sensor_queries.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pymongo.collection import Collection
 
 from multi_modal_edge_ai.client.databases.sensor_database_preprocessing_methods import *
@@ -70,26 +72,62 @@ def get_all_documents_all_fields(collection: Collection) -> list[dict[Any, Any]]
     return data
 
 
-def get_past_x_seconds_of_all_sensor_entries(collection: Collection, x: int, current_time: datetime.datetime) \
-        -> list[dict[Any, Any]]:
+# def get_past_x_seconds_of_all_sensor_entries(collection: Collection, x: int, current_time: datetime.datetime) \
+#         -> list[dict[Any, Any]]:
+#     """
+#     A method that retrieves all sensor entries from the local collection using a local method. It then returns
+#     the entries that have a start_time no longer than x seconds ago.
+#     :param collection: the collection to be queried
+#     :param current_time: the current time as datetime
+#     :param x: the amount of seconds of sensor date to be returned
+#     :return: all the entries that have a start_time no longer than x seconds ago
+#     """
+#     print(current_time)
+#     data = get_all_documents(collection)
+#     print("data", data)
+#     new_data = []
+#
+#     for entry in data:
+#         entry_date = datetime.datetime.strptime(entry['date'], '%Y-%m-%d')
+#         entry_time = datetime.datetime.strptime(entry['start_time'], '%H:%M:%S').time()
+#         entry_datetime = datetime.datetime.combine(entry_date, entry_time)
+#
+#         time_difference = current_time - entry_datetime
+#         if time_difference.total_seconds() <= x:
+#             new_data.append(entry)
+#     return new_data
+
+
+def get_past_x_seconds_of_all_sensor_entries(collection: Collection, x: int, current_time: datetime.datetime) -> list[dict]:
     """
-    A method that retrieves all sensor entries from the local collection using a local method. It then returns
-    the entries that have a start_time no longer than x seconds ago.
-    :param collection: the collection to be queried
-    :param current_time: the current time as datetime
-    :param x: the amount of seconds of sensor date to be returned
-    :return: all the entries that have a start_time no longer than x seconds ago
+    A method that retrieves entries from the collection where 'last_seen' is greater than or equal to
+    the current time minus x seconds.
+
+    :param collection: The collection to be queried.
+    :param current_time: The current time as a datetime object.
+    :param x: The number of seconds in the past to retrieve data from.
+    :return: A list of dictionaries.
     """
-    data = get_all_documents(collection)
-    new_data = []
+    query_time = (current_time - datetime.timedelta(seconds=x)).timestamp() * 1000
 
-    for entry in data:
-        entry_date = datetime.datetime.strptime(entry['date'], '%Y-%m-%d')
-        entry_time = datetime.datetime.strptime(entry['start_time'], '%H:%M:%S').time()
-        entry_datetime = datetime.datetime.combine(entry_date, entry_time)
+    power_sensors = filter_by_time_and_name(collection, query_time, "power_")
+    contact_sensors = filter_by_time_and_name(collection, query_time, "contact_")
+    motion_sensors = filter_by_time_and_name(collection, query_time, "motion_")
 
-        time_difference = current_time - entry_datetime
+    # processing sensors
+    processed_power_sensors = aggregate_similar_entries(get_data_from_cursor(power_sensors, 'Power'), 60)
+    processed_contact_sensors = aggregate_similar_entries(get_data_from_cursor(contact_sensors, "Contact"), 60)
+    processed_pir_sensors = aggregate_similar_entries(get_data_from_cursor(motion_sensors, "PIR"), 60)
 
-        if time_difference.total_seconds() <= x:
-            new_data.append(entry)
-    return new_data
+    return processed_power_sensors + processed_contact_sensors + processed_pir_sensors
+# def filter_entries_by_friendly_name(cursor, search_string):
+#     collection = cursor.with_options(cursor_type=Collection)
+#     filtered_cursor = collection.find({'device.friendlyName': {'$regex': search_string}})
+#     return filtered_cursor
+
+
+def filter_by_time_and_name(collection, query_time, regex):
+    return collection.find({
+        'last_seen': {'$gt': query_time},
+        'device.friendlyName': {'$regex': regex}
+    })
--- tests/client/databases/test_integration_sensor_queries.py
+++ tests/client/databases/test_integration_sensor_queries.py
@@ -4,18 +4,18 @@ from multi_modal_edge_ai.client.databases.database_connection import *
 from multi_modal_edge_ai.client.databases.sensor_queries import *
 
 
-def test_get_sensor_data_from_x_minutes_ago_one_entry():
-    collection = get_collection(get_database(get_database_client(), "coho-edge-ai-test"), "data")
-    current_date = datetime.datetime(2023, 6, 12, 11, 30, 0)
-    minutes = int((datetime.datetime(2023, 6, 12, 11, 30, 0).timestamp()
-                   - datetime.datetime(2023, 4, 26, 16, 22, 30).timestamp()) / 60)
-    entries = get_past_x_seconds_of_all_sensor_entries(collection, minutes * 60, current_date)
-    assert len(entries) == 1
-    assert entries[0]["date"] == '2023-04-26'
-    assert entries[0]["start_time"] == '17:22:26'
-    assert entries[0]["end_time"] == '17:23:17'
-
-    assert entries[0]['device']["friendlyName"] == 'power_tv'
+# def test_get_sensor_data_from_x_minutes_ago_one_entry():
+#     collection = get_collection(get_database(get_database_client(), "coho-edge-ai-test"), "data")
+#     current_date = datetime.datetime(2023, 6, 12, 11, 30, 0)
+#     minutes = int((datetime.datetime(2023, 6, 12, 11, 30, 0).timestamp()
+#                    - datetime.datetime(2023, 4, 26, 16, 22, 30).timestamp()) / 60)
+#     entries = get_past_x_seconds_of_all_sensor_entries(collection, minutes * 60, current_date)
+#     assert len(entries) == 1
+#     assert entries[0]["date"] == '2023-04-26'
+#     assert entries[0]["start_time"] == '17:22:26'
+#     assert entries[0]["end_time"] == '17:23:17'
+#
+#     assert entries[0]['device']["friendlyName"] == 'power_tv'
 
 
 def test_get_sensor_data_from_x_minutes_ago_no_entries():
